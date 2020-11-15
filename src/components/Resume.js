30dbf4de0ee71ce4d53439ab691bf4f223b8b8e7
ReginaF2012 <skritschy@gmail.com>
2020-11-14 18:50:11 -0600

    resume widget not working, adding script tag to Resume.js to see if that fixes it

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Resume(){
     return (
         <div className='section resume'>
+            <script src="https://apps.elfsight.com/p/platform.js" defer></script>
             <div class="elfsight-app-718e2e90-d7aa-4a49-8dc1-4fb5cbce22d6"></div>
         </div>
     )
